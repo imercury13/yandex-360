efb7383564b005b8a3f741c3d2eb7208f53c1162
Igor Kuptsov <i.kuptsov@uh.net.ru>
2022-07-25 12:42:57 +0300

    init

---
+++ README.rst
@@ -0,0 +1,7 @@
+**Yandex OAuth Lib**
+
+```
+pip install yandex-oauth
+```
+
+`Документация по библиотеке <https://yandex-oauth.readthedocs.io/>`_
---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+from os.path import join, dirname
+import yandex_360
+
+setup(
+    name='yandex_360',
+    version=yandex_360.__version__,
+    packages=find_packages(),
+    description='Yandex 360 Lib',
+    long_description=open(join(dirname(__file__), 'README.rst')).read(),
+    author=yandex_360.__author__,
+    author_email='ya360@uh.net.ru',
+    maintainer=yandex_360.__author__,
+    maintainer_email='ya360@uh.net.ru',
+    download_url='https://github.com/imercury13/yandex_360',
+    #url='https://ya360.uh.net.ru',
+    license='MIT',
+    project_urls={
+        "Documentation": "https://yandex-360.readthedocs.io/",
+        "Bug Tracker": "https://github.com/imercury13/yandex_360/issues"
+    },
+    classifiers=[
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python'
+    ],
+    install_requires=[
+        'jreq',
+    ],
+    include_package_data=True,
+)
