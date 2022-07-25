1fca210b1fbaa42067fbb7eab09fe6d49e52ce3b
Igor Kuptsov <i.kuptsov@uh.net.ru>
2022-07-25 14:57:20 +0300

    func

--- yandex_360/ya360.py
+++ yandex_360/ya360.py
@@ -53,3 +53,34 @@ def delete_member_group(token, orgID, ID, userType, userID):
     headers={'Authorization': 'OAuth '+token, 'Content-type': 'application/json'}
 
     return safe_request('delete', url, headers)
+
+def show_group(token, orgID, ID):
+    """Функция отображения информации о группе
+	
+    """
+
+    url = 'https://api360.yandex.net/directory/v1/org/'+orgID+'/groups/'+ID+'/'
+    headers={'Authorization': 'OAuth '+token, 'Content-type': 'application/json'}
+    
+    return safe_request('get', url, headers)
+
+def show_members_group(token, orgID, ID):
+    """Функция отображения членов группы
+	
+    """
+
+    url = 'https://api360.yandex.net/directory/v1/org/'+orgID+'/groups/'+ID+'/members/'
+    headers={'Authorization': 'OAuth '+token, 'Content-type': 'application/json'}
+    
+    return safe_request('get', url, headers)
+
+def show_groups(token, orgID, url=None):
+    """Функция вывода списка всех групп
+
+    """
+
+    url = 'https://api360.yandex.net/directory/v1/org/'+orgID+'/groups/?'+url
+    headers={'Authorization': 'OAuth '+token, 'Content-type': 'application/json'}
+	
+    return safe_request('get', url, headers)
+
