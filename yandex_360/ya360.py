e9e3f331432c60900bccfbbb6f345d5d993579c6
Igor Kuptsov <i.kuptsov@uh.net.ru>
2022-07-25 14:15:50 +0300

    func

--- yandex_360/ya360.py
+++ yandex_360/ya360.py
@@ -1,32 +1,55 @@
 """Модуль функций библиотеки"""
 
+from wsgiref import headers
 from jreq.jreq import safe_request
 
-def _check_request(req):
-	"""Функция проверки ответа запроса
-	
-	:param req: результат запроса
-	:type req: dict
-	"""
-	if 'code' in req and 'message' in req:
-		print('Код ошибки: '+str(req['code'])+' Сообщение: '+req['message'])
-		exit(1)
-
-def create_group(args, token, orgID):
+def create_group(token, orgID, body):
     """Функция создания группы
 
-    :param args: словарь аргументов командной строки
-    :type args: dict
     """
 
     url = 'https://api360.yandex.net/directory/v1/org/'+orgID+'/groups/'
     headers={'Authorization': 'OAuth '+token, 'Content-type': 'application/json'}
-    body = {}
-    body.update({'name':args.name})
-    if args.label: body.update({'label':args.label})
-    if args.adminIds: body.update({'adminIds':args.adminIds})
-    if args.description: body.update({'description':args.description})
-    cg = safe_request('post', url, headers, body)
-    _check_request(cg)
-    print('Группа создана с ID: '+str(cg['id']))
+    
+    return  safe_request('post', url, headers, body)
 
+def update_group(token, orgID, body, ID):
+	"""Функция обновления информации о группе
+	
+	"""
+
+	url = 'https://api360.yandex.net/directory/v1/org/'+orgID+'/groups/'+ID+'/'
+	headers={'Authorization': 'OAuth '+token, 'Content-type': 'application/json'}
+
+	return safe_request('patch', url, headers, body)
+
+def delete_group(token, orgID, ID):
+    """Функция удаления группы
+    (необратимая операция)
+
+    """
+
+    url = 'https://api360.yandex.net/directory/v1/org/'+orgID+'/groups/'+ID+'/'
+    headers={'Authorization': 'OAuth '+token, 'Content-type': 'application/json'}
+
+    return safe_request('delete', url, headers)
+
+def add_member_group(token, orgID, body, ID):
+    """Функция добавления участника в группу
+
+    """
+
+    url = 'https://api360.yandex.net/directory/v1/org/'+orgID+'/groups/'+ID+'/members'
+    headers={'Authorization': 'OAuth '+token, 'Content-type': 'application/json'}
+
+    return safe_request('post', url, headers, body)
+
+def delete_member_group(token, orgID, ID, userType, userID):
+    """Функция удаления участника из группы
+
+    """
+
+    url = 'https://api360.yandex.net/directory/v1/org/'+orgID+'/groups/'+ID+'/members/'+userType+'/'+userID
+    headers={'Authorization': 'OAuth '+token, 'Content-type': 'application/json'}
+
+    return safe_request('delete', url, headers)
