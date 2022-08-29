f7c37544b48f45490de35eb7e872eed69a36cd17
Igor Kuptsov <i.kuptsov@uh.net.ru>
2022-08-29 09:26:59 +0300

    add antipspam show whitelist

--- yandex_360/antispam.py
+++ yandex_360/antispam.py
@@ -2,3 +2,21 @@
 
 from jreq.jreq import safe_request
 import json
+
+def show_whitelist(token, orgID):
+    '''Функция вывода содержимого белого списка
+    
+    :param token: :term:`Яндекс токен приложения`
+    :type token: str
+    :param orgID: :term:`ID организации в Яндекс 360`
+    :type orgID: str
+    :return: результат запроса
+    :rtype: dict
+
+    '''
+
+    url = 'https://api360.yandex.net/admin/v1/org/'+orgID+'/mail/antispam/allowlist/ips'
+    headers={'Authorization': 'OAuth '+token, 'Content-type': 'application/json'}
+
+    return safe_request('get', url, headers)
+
