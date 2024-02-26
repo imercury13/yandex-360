1bcfa19dea359f9eb05c0639d62e7eca73f45096
Igor Kuptsov <108464106+imercury13@users.noreply.github.com>
2024-02-26 20:26:48 +0300

    Добавление модуля mail Постепенный переход на f строки

---
+++ yandex_360/mail.py
@@ -0,0 +1,22 @@
+"""Модуль функций mail (26.02.2024)
+для работы с настройками почтовых ящиков сотрудников. Работает для пользователей, аккаунты которых созданы на домене организации.
+
+Разрешения на использование сервиса, которые доступны при настройке приложения:
+
+ya360_admin:mail_read_user_settings — чтение настроек почты пользователя;
+ya360_admin:mail_write_user_settings — управление настройками почты пользователя.
+"""
+
+from jreq.jreq import safe_request
+
+def show_mail_signs (token, orgID, userID):
+    """Функция позволяет просмотреть почтовый адрес, с которого отправляются письма по умолчанию, и настройки подписей сотрудника
+
+    :return: результат запроса
+    :rtype: dict
+    """
+
+    url = f'https://api360.yandex.net/admin/v1/org/{orgID}/mail/users/{userID}/settings/sender_info'
+    headers={'Authorization': f'OAuth {token}', 'Content-type': 'application/json'}
+
+    return safe_request('get',url, headers)
