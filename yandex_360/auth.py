33d242b564ee73135a68b209a16f9d6845b38385
Купцов Игорь Валерьевич <i.kuptsov@norgau.com>
2024-03-19 11:20:38 +0300

    show domain session

--- yandex_360/auth.py
+++ yandex_360/auth.py
@@ -12,3 +12,28 @@
 from jreq.jreq import safe_request
 import json
 
+def show_domain_sessions(token, orgID):
+    """Функция возвращает время жизни cookie сессий пользователей организации.
+    Значение по умолчанию — 0. Это значит, что время жизни cookie сессий не ограничено.
+
+    :param token: :term:`Яндекс токен приложения`
+    :type token: str
+    :param orgID: :term:`ID организации в Яндекс 360`
+    :type orgID: str
+    :return: :numref:`результат запроса %s <Результат запроса show_domain_sessions>`
+    :rtype: dict
+
+    .. code-block:: python
+        :caption: Результат запроса show_domain_sessions
+        :name: Результат запроса show_domain_sessions
+
+        {
+            "authTTL": int
+        }
+
+    """
+
+    url = f'https://api360.yandex.net/security/v1/org/{orgID}/domain_2fa'
+    headers={'Authorization': f'OAuth {token}', 'Content-type': 'application/json'}
+	
+    return safe_request('get', url, headers)
