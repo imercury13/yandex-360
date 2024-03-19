e463dc95428a21488b601f4f804d76d8e552a1d6
Купцов Игорь Валерьевич <i.kuptsov@norgau.com>
2024-03-19 11:29:57 +0300

    domain sessions

--- yandex_360/auth.py
+++ yandex_360/auth.py
@@ -33,7 +33,47 @@ def show_domain_sessions(token, orgID):
 
     """
 
-    url = f'https://api360.yandex.net/security/v1/org/{orgID}/domain_2fa'
+    url = f'https://api360.yandex.net/security/v1/org/{orgID}/domain_sessions'
     headers={'Authorization': f'OAuth {token}', 'Content-type': 'application/json'}
 	
     return safe_request('get', url, headers)
+
+def edit_domain_sessions (token, orgID, body):
+    """Функция позволяет возможность поменять время жизни cookie сессий пользователей организации.
+
+    ..danger::
+        Ограничение. Чтобы выполнить запрос, приложению требуется разрешение на управление временем жизни cookie сессий пользователей и авторизацией.
+
+
+    :param token: :term:`Яндекс токен приложения`
+    :type token: str
+    :param orgID: :term:`ID организации в Яндекс 360`
+    :type orgID: str
+    :param body: :numref:`тело запроса %s <Тело запроса edit_domain_sessions>`
+    :type body: dict
+    :return: :numref:`результат запроса %s <Результат запроса edit_domain_sessions>`
+    :rtype: dict
+
+
+    .. code-block:: python
+        :caption: Тело запроса edit_domain_sessions
+        :name: Тело запроса edit_domain_sessions
+
+        {
+            "authTTL": int
+        }
+
+    .. code-block:: python
+        :caption: Результат запроса edit_domain_sessions
+        :name: Результат запроса edit_domain_sessions
+
+        {
+            "authTTL": int
+        }
+
+    """
+
+    url = f'https://api360.yandex.net/security/v1/org/{orgID}/domain_sessions'
+    headers={'Authorization': f'OAuth {token}', 'Content-type': 'application/json'}
+
+    return safe_request('post',url, headers, json.dumps(body))
