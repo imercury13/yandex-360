0f62c44041af462e195f7afaa02d7ef4a19ea668
Купцов Игорь Валерьевич <i.kuptsov@norgau.com>
2024-03-21 09:32:47 +0300

    init

--- docs/source/yandex_360.rst
+++ docs/source/yandex_360.rst
@@ -120,11 +120,3 @@
    :members:
    :undoc-members:
    :show-inheritance:
-
-Модуль yandex\_360.ya360
-------------------------
-
-.. automodule:: yandex_360.ya360
-   :members:
-   :undoc-members:
-   :show-inheritance:
--- yandex_360/tools.py
+++ yandex_360/tools.py
@@ -88,3 +88,69 @@ def get_id_user_by_nickname(sstr, token, orgID):
             usrs = users.show_users(token, orgID, page=usrs['page']+1)
     else:
         return usrs
+
+def get_groups(token, orgID):
+    """Функция вывода всех групп
+    
+    :param token: :term:`Яндекс токен приложения`
+    :type token: str
+    :param orgID: :term:`ID организации в Яндекс 360`
+    :type orgID: str
+    :return: словарь групп
+    :rtype: dict
+    
+    """
+
+    lst_grp =[]
+
+    grps = groups.show_groups(token, orgID)
+    if check_request(grps):
+        while grps['page'] <= grps['pages']:
+            lst_grp.append(grps['groups'])
+            grps = groups.show_groups(token, orgID, page=grps['page']+1)
+    else:
+        return {"groups":lst_grp,"page":grps['page'],"pages":grps['pages'],"perPage":grps['perPage'],"total":grps['total']}
+
+def get_departments(token, orgID):
+    """Функция вывода всех подразделений
+    
+    :param token: :term:`Яндекс токен приложения`
+    :type token: str
+    :param orgID: :term:`ID организации в Яндекс 360`
+    :type orgID: str
+    :return: словарь подразделений
+    :rtype: dict
+    
+    """
+
+    lst_dep =[]
+
+    deps = departments.show_departments(token, orgID)
+    if check_request(deps):
+        while deps['page'] <= deps['pages']:
+            lst_dep.append(deps['departments'])
+            deps = departments.show_departmets(token, orgID, page=deps['page']+1)
+    else:
+        return {"groups":lst_dep,"page":deps['page'],"pages":deps['pages'],"perPage":deps['perPage'],"total":deps['total']}
+
+def get_users(token, orgID):
+    """Функция вывода всех пользователей
+    
+    :param token: :term:`Яндекс токен приложения`
+    :type token: str
+    :param orgID: :term:`ID организации в Яндекс 360`
+    :type orgID: str
+    :return: словарь пользователей
+    :rtype: dict
+    
+    """
+
+    lst_usr =[]
+
+    usrs = usrs.show_users(token, orgID)
+    if check_request(usrs):
+        while usrs['page'] <= usrs['pages']:
+            lst_usr.append(users['users'])
+            usrs = groups.show_users(token, orgID, page=usrs['page']+1)
+    else:
+        return {"groups":lst_usr,"page":usrs['page'],"pages":usrs['pages'],"perPage":usrs['perPage'],"total":usrs['total']}
